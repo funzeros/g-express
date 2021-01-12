131f29d0725556e3ceaf1abc0af76aa2848560db
方珂琛 <984512789@qq.com>
2021-01-12 23:14:17 +0800

    添加swagger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import "dotenv/config";
+import path from "path";
 import express from "express";
 import expressWs from "express-ws";
 import bodyParser from "body-parser"; //用于req.body获取值的
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./static/swagger.json";
 import scheduleWs from "./util/autoSchedule";
 import moduleRouter from "./module/index";
 import {not_found_handler, error_handler_middleware} from "./util/middle";
@@ -11,6 +14,7 @@ import {connectDB} from "./schema/index";
 connectDB();
 // express
 const appBase = express();
+
 // websocket
 const wsInstance = expressWs(appBase);
 const {app} = wsInstance;
@@ -23,16 +27,18 @@ app.ws("/autoSchedule", ws => {
 app.use(bodyParser.json());
 // 创建 application/x-www-form-urlencoded 编码解析
 app.use(bodyParser.urlencoded({extended: false}));
+app.use("/ws", express.static(path.join(__dirname, "static")));
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // 加载模块
+const prefix = process.env.PREFIX || "/api";
 Object.entries(moduleRouter).forEach(([name, module]) => {
-  app.use("/" + name, module);
+  app.use(prefix + "/" + name, module);
 });
 
 // 后置中间件
 app.use(not_found_handler); // 先判断此异常
 app.use(error_handler_middleware); // 最后判断此异常
-
 const PORT = process.env.PORT || 10045;
 app.listen(PORT, () => {
   console.log(`服务已启动--> http://127.0.0.1:${PORT}`);
--- src/util/middle.ts
+++ src/util/middle.ts
@@ -1,18 +1,15 @@
+import {DTO} from "../module/types";
 // 中间件
 const error_handler_middleware = (err: any, req: any, res: any): void => {
   if (err) {
     const {message} = err;
     res.status(500);
-    res.json({
-      message: `${message || "服务器异常"}`,
-    });
+    DTO.error(res)(`${message || "服务器异常"}`);
   }
 };
 // 404处理
-const not_found_handler = (req: unknown, res: any): void => {
-  res.json({
-    message: "api不存在",
-  });
+const not_found_handler = (req: any, res: any): void => {
+  DTO.error(res)("api不存在");
 };
 
 export {error_handler_middleware, not_found_handler};
