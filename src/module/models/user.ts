787b39a998f2d1d72ae21f39aada2249fa1d4103
方珂琛 <984512789@qq.com>
2021-01-16 21:47:08 +0800

    更新登录注册方法

--- src/module/models/user.ts
+++ src/module/models/user.ts
@@ -1,8 +1,7 @@
 import {router} from "../router";
 import {DTO} from "../types";
-import schema from "../../schema/index";
 import {validType} from "../../util/util";
-const user = schema.user.default;
+import user, {getUserInfo, getCountByName, updateToken} from "../../schema/models/user";
 
 /**
  * 注册
@@ -13,16 +12,45 @@ router.post("/register", async (req, res) => {
     password: String,
   });
   if (valid.f) {
-    const amount = await user.count({
-      where: {
-        name: valid.resData.name,
-      },
-    });
+    const amount = await getCountByName(valid.resData.name);
     if (amount) return DTO.error(res)("该用户名已注册");
-    const data = await user.create(valid.resData);
-    return DTO.data(res)(data);
+    try {
+      await user.create(valid.resData);
+      return DTO.data(res)(true);
+    } catch {
+      return DTO.error(res)("注册失败");
+    }
   }
   return DTO.error(res)(valid.err);
 });
 
+/**
+ * 登录
+ */
+router.post("/login", async (req, res) => {
+  const valid: any = await validType(req.body, {
+    name: String,
+    password: String,
+  });
+  if (valid.f) {
+    try {
+      const data: any = await getUserInfo({
+        name: valid.resData.name,
+        password: valid.resData.password,
+      });
+      if (data) {
+        await updateToken(data.id);
+        const userInfo: any = await getUserInfo({
+          id: data.id,
+        });
+        return DTO.data(res)(userInfo);
+      } else {
+        return DTO.error(res)("用户名或密码错误");
+      }
+    } catch {
+      return DTO.error(res)("登录失败");
+    }
+  }
+  return DTO.error(res)(valid.err);
+});
 export default router;
--- src/schema/models/user.ts
+++ src/schema/models/user.ts
@@ -1,6 +1,54 @@
 import {STRING} from "sequelize";
+import {v4 as uuidv4} from "uuid";
+
 import {defineModel} from "../db";
-export default defineModel("user", {
-  name: STRING(10),
-  password: STRING(40),
+const user = defineModel("user", {
+  name: STRING(32),
+  password: STRING(32),
+  token: {
+    allowNull: true,
+    type: STRING(36),
+  },
 });
+
+/**
+ * 根据条件查询用户信息
+ * @param name
+ * @param password
+ */
+export const getUserInfo = async (where: any) => {
+  const data = await user.findAll({
+    attributes: ["id", "name", "token"],
+    where,
+  });
+  return data[0];
+};
+/**
+ * 根据名字查数量
+ * @param name
+ */
+export const getCountByName = async (name: string) => {
+  return await user.count({
+    where: {
+      name: name,
+    },
+  });
+};
+
+/**
+ * 更新token
+ * @param id
+ */
+export const updateToken = async (id: string) => {
+  const token = uuidv4();
+  return await user.update(
+    {token},
+    {
+      where: {
+        id,
+      },
+    }
+  );
+};
+
+export default user;
